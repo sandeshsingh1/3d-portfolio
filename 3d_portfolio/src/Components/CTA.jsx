ca7aadd8c6b6b6ec49766542fac1d8709171d169
Sandesh SIngh <sandeshsingh201106@gmail.com>
2025-12-27 00:29:55 +0530

    Added resume

--- 3d_portfolio/src/Components/CTA.jsx
+++ 3d_portfolio/src/Components/CTA.jsx
@@ -1,17 +1,26 @@
+import resume from "../assets/resume/Sandesh-Singh-Resume.pdf";
 import { Link } from "react-router-dom";
 
 const CTA = () => {
   return (
-    <section className='cta'>
-      <p className='cta-text'>
-        Have a project in mind? <br className='sm:block hidden' />
+    <section className="cta">
+      <p className="cta-text">
+        Have a project in mind? <br className="sm:block hidden" />
         Let’s build something together!
       </p>
-      <Link to='/contact' className='btn'>
-        Contact
-      </Link>
+
+      <div className="flex gap-4 flex-wrap">
+        {/* Contact Button */}
+        <Link to="/contact" className="btn">
+          Contact
+        </Link>
+
+        {/* Resume Download Button */}
+        <a href={resume} download className="btn btn-outline">
+          Download Resume
+        </a>
+      </div>
     </section>
   );
 };
-
 export default CTA;
